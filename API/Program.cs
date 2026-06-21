23c9ceffc8474ba8ec0025dc413abd757ef68bbb
Sudhanshu Ranjan <sr93344@gmail.com>
2026-06-21 21:39:31 +0530

    Angular Skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.Run();
---
+++ client/src/app/app.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+  protected readonly title = signal('Tinder');
+  protected members = signal<any[]>([]);
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe((data) => {
+      this.members.set(data as any[]);
+    });
+  }
+
+}
