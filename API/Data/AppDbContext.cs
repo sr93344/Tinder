bcadd0fe181dbbc32eea714b72917dca6464e715
Sudhanshu Ranjan <sr93344@gmail.com>
2026-06-21 17:23:03 +0530

    Add project files.

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
